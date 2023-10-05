40d2bc648e3027fab05378e4357349401c0718e1
oscarburbano278 <oscar10181@hotmail.com>
2023-10-05 15:03:26 -0500

    whith a builder  we can have many objects

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("we are conneted ?")
+// constructor de objetos en este caso de Shoes
+
+function Shoes (estilo, color, talla, existencia){
+    this.estilo = estilo;
+    this.color = color;
+    this.talla = talla;
+    this.existencia = existencia;
+};
+
+// construccion de objetos a partir de un constructor en este caso "Shoes" 
+
+var shoes1 = new Shoes("casual", "cafe", 40, true);
+console.log(shoes1);
+var shoes2 = new Shoes("escolar", "negro", 36, false);
+console.log(shoes2);
+
