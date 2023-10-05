d112a4cefa876288f999fc62ef0e3077ba23b20f
oscarburbano278 <oscar10181@hotmail.com>
2023-10-05 14:45:30 -0500

    normal objects construction

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log("we are conneted ?")
+//console.log("we are conneted ?")
+
+
+//creacion y muestra en consola de objetos
+    var  shoes = {
+        estyle: "sports",
+        color: ["black","white"],
+        size: 38,
+        existence: true
+    };
+
+    var user ={
+        id: 1234,
+        name: "Oscar",
+        lastName: "Burbano",
+    }        
+
+    console.log(shoes);
+    console.log(user);
+
+
