4a53868381f7b9b17b109ed41866b2bb473196d2
oscarburbano278 <oscar10181@hotmail.com>
2023-10-05 15:52:52 -0500

    ways to access object attributes

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 // constructor de objetos en este caso de Shoes
 
-function Shoes (estilo, color, talla, existencia){
+function Shoes (estilo, color, talla, existencia, nationality){
     this.estilo = estilo;
     this.color = color;
     this.talla = talla;
     this.existencia = existencia;
+    this.nationality = nationality;
 };
 
 // construccion de objetos a partir de un constructor en este caso "Shoes" 
 
-var shoes1 = new Shoes("casual", "cafe", 40, true);
+var shoes1 = new Shoes("casual", "cafe", 40, true, ["Colombia", "Mexico"]);
 console.log(shoes1);
 var shoes2 = new Shoes("escolar", "negro", 36, false);
 console.log(shoes2);
@@ -33,3 +34,13 @@ var users = [
     }
 ]
 console.log(users);
+
+// formas de acceder a los atributos de los objetos con respecto a Shoes
+
+console.log(shoes1.color);
+//con corchetes
+console.log(shoes1["color"]);
+// cuando un atributo es un array
+console.log(shoes1["nationality"][1]);
+// sin corchetes
+console.log(shoes1.nationality[0]);
